749307577ee328f39186b12df12498b066d278f2
matheus227 <matheus.secundario27@gmail.com>
2026-06-01 22:19:04 -0300

    upload para o vercel

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('startBtn').addEventListener('click',()=>{
+  alert('Explorando o mapa Catch Buddy!');
+});
+
+document.getElementById("editarPerfil").addEventListener("click", () => {
+    window.location.href = "editarperfil.html";
+});
